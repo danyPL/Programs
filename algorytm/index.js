c339626c456ddee705178fd1a2edc4d86de264b7
danyPL <112093587+danyPL@users.noreply.github.com>
2022-09-22 15:53:00 +0200

    Add files via upload

---
+++ algorytm/index.js
@@ -0,0 +1,22 @@
+const eps = 0.000001;
+function pierwiastek(P, eps){
+    P = prompt("Podaj liczbę z której chcesz wyznaczyć pierwiastek");
+    var a = 1, b = P;
+    while(Math.abs(a-b)>=eps){
+        a = (a+b)/2;
+        b = P/a;
+    }
+    return console.log(parseFloat((a+b)/2));
+}
+
+
+function wypisz(){
+    let pole1 = document.querySelector("#pole1").value;
+    let pole2 = document.querySelector("#pole2").value;
+    let text = " ";
+    for(i=pole1;i<=pole2;i++){
+        text = text + i + " ";
+    }
+        document.querySelector("#wynik").innerHTML = text + " ";
+
+}
